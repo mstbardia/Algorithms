8253f2242642ccc55e86d709ff083b998e4ed488
Bardia Mostafavi <mstbardia@gmail.com>
2023-05-26 01:26:54 +0430

    bubble sort added

---
+++ Examples/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Algorithms.Examples;
+
+public static class Extensions
+{
+    public static string ToJson(this int[] inp)
+    {
+        return JsonSerializer.Serialize(inp);
+    }
+}
